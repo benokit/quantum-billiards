f6e2f3ded5348d3c013ff546ce36c65e648c5bc9
benokit <benjamin.batistic@gmail.com>
2019-02-08 18:57:53 +0100

    Husimi lambda

--- src/robnikBilliard.py
+++ src/robnikBilliard.py
@@ -11,6 +11,7 @@ import numpy as np
 import verginiSaraceno as vs
 import spectrumUtilities as su
 import scipy.integrate as integrate
+import husimiFunctions as hf
 
 def robnikBoundaryDefinition(lam, k, mC = None):
     if (mC == None):
@@ -33,11 +34,10 @@ def robnikBoundaryDefinitionExtended(lam, k, mC = None):
     y = np.sin(alpha) + lam * np.sin(2 * alpha)
     dsdalpha = np.sqrt(1 + 4 * lam * (lam + np.cos(alpha)))
     nx = (np.cos(alpha) + 2 * lam * np.cos(2 * alpha)) / dsdalpha
-    ny = (np.sin(alpha) + 2 * lam * np.sin(2 * alpha)) / dsdalpha
-    q = 1 + 4 * lam * lam + 4 * lam * np.cos(alpha)
-    z = 1 + 2 * lam * lam + 3 * lam * np.cos(alpha)
-    w = dalpha * q / z
-    return x, y, w
+    ny = (np.sin(alpha) + 2 * lam * np.cos(2 * alpha)) / dsdalpha
+    ds = dsdalpha * dalpha
+    s = np.cumsum(ds) - 0.5 * ds
+    return x, y, nx, ny, s, ds
 
 """
 asym
@@ -100,6 +100,24 @@ def robnikSpectrumUnfold_sym(lam, energies):
     Shor = 2.0
     S = Scurve - Shor
     return su.unfoldWail(A, S, energies)
+
+def robnikHusimi_sym(lam, k, vec):
+    x, y, nx, ny, s, ds = robnikBoundaryDefinitionExtended(lam, k)
+    dpsi_x, dpsi_y = vs.grad_psi_pi_sym(k, vec, x, y)
+    u = dpsi_x * nx + dpsi_y * ny
+    Scurve = integrate.quad(lambda z: m.sqrt(1 + 4 * lam * (lam + m.cos(z))), 0, m.pi)[0]
+    s1 = np.concatenate((s, 2 * Scurve - np.flipud(s)))
+    s2 = np.concatenate((-np.flipud(s), s1))
+    ds1 = np.concatenate((ds, np.flipud(ds)))
+    ds2 = np.concatenate((np.flipud(ds), ds1))
+    u1 = np.concatenate((u, np.flipud(u)))
+    u2 = np.concatenate((np.flipud(u), u1))
+    nQ = 400
+    nP = 400
+    qs = vs.baseAngles(0, Scurve, nQ)
+    ps = vs.baseAngles(0, 1, nP)
+    H = hf.husimiOnGrid(k, s2, ds2, u2, qs, ps)
+    return qs, ps, H
 
 
     
